41c1c1e02910ee0e5726e226e226c56f9c64eb03
Richard Rigby <r.rigby@leeds.ac.uk>
2019-04-30 12:23:48 +0100

    Initial commit for GitHub

---
+++ test.m
@@ -0,0 +1,20 @@
+% Test LEROI_TEM
+
+% The input below is equivalent to the Leroi.cfl content:
+%
+% 3 0 3                 !! NLYR, NPLT, NLITH
+% 9.9773 -1 1 1 0 0 1
+% 2035.84 -1 1 1 0 0 1 
+% 887.959 -1 1 1 0 0 1  !! LYTH(NLITH, 7)
+% 1 94.2462
+% 2 34.1019             !! LITHL(NLYR - 1), THK(NLYR - 1)
+% 3                     !! LITH(NLYR) 
+
+NLYR  = 3;
+NPLT  = 0;
+NLITH = 3;
+LYTH  = [9.9773, -1, 1, 1, 0, 0, 1; 2035.84, -1, 1, 1, 0, 0, 1; 887.959, -1, 1, 1, 0, 0, 1];
+LITHL = [1, 2, 3];
+THK   = [94.2462, 34.1019];
+
+LEROI_TEM(NLYR, NPLT, NLITH, LYTH, LITHL, THK)
